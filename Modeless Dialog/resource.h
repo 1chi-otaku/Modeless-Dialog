c3444509484b3c34855a15f35d69867fadc3930c
Serhii <mywaifuqueen@gmail.com>
2022-11-14 11:58:25 +0000

    Add project files.

---
+++ Modeless Dialog/resource.h
@@ -0,0 +1,43 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Modeless Dialog.rc
+//
+#define IDD_DIALOG2                     101
+#define IDD_DIALOG1                     103
+#define IDD_DIALOG3                     105
+#define IDD_DIALOG4                     107
+#define IDC_EDIT1                       1001
+#define IDC_STATIC1                     1002
+#define IDC_EDIT3                       1002
+#define IDC_STATIC2                     1003
+#define IDC_EDIT4                       1003
+#define IDC_EDIT2                       1004
+#define IDC_EDIT6                       1004
+#define IDC_EDIT7                       1005
+#define IDC_BUTTON2                     1006
+#define IDC_BUTTON1                     1006
+#define IDC_EDIT8                       1006
+#define IDC_BUTTON3                     1007
+#define IDC_EDIT9                       1007
+#define IDC_BUTTON4                     1008
+#define IDC_EDIT10                      1008
+#define IDC_COMBO1                      1009
+#define IDC_RADIO1                      1010
+#define IDC_RADIO2                      1011
+#define IDC_EDIT5                       1012
+#define IDC_CHECK1                      1013
+#define IDC_CHECK2                      1014
+#define IDC_CHECK3                      1015
+#define IDC_CHECK4                      1016
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        109
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1014
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
